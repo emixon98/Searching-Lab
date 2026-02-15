4dbe2783cfbbf7ec79d597751186ee387962b858
Elijah Mixon <emixon98@gmail.com>
2026-02-14 23:42:32 -0800

    Basic linear and binary search algorithms finished

--- main.cpp
+++ main.cpp
@@ -1,4 +1,57 @@
+#include <iostream>
+using namespace std;
+
+static const int N = 100000;
+
+//linear is straight forward, using & so I can return comparisons in main
+int linear_Search(int arr[], int n, int search, double& comparisons){
+    comparisons = 0;
+    for (int i = 0; i < n; i++){
+        comparisons++;
+        if(arr[i] == search) return i;
+    }
+    return -1;
+}
+
+int binary_Search(int arr[], int n, int search, int& comparisons){
+    comparisons = 0;
+    int low = 0, high = n-1; //setting initial indexes for search
+
+    while (low <= high){
+        int mid = low + (high - low) / 2; //consistent middle
+        comparisons++;
+        if (arr[mid] == search) return mid;
+
+        comparisons++; //this is technically a comparison, so incrementing here too
+        if(search < arr[mid])
+            high = mid -1;
+        else 
+            low = mid +1;
+    }
+    return -1;
+}
+
 int main() {
+    int testArr[N];
+
+    //binary needs sorted, so going to just match element to it's index
+    for(int i=0; i < N; i++) testArr[i] = i;
+
+    int testVal;
+    cout << "Enter a whole number between 0 - 99999 to search for: ";
+    cin >> testVal;
+
+    double linearComparisons = 0;
+    int binaryComparisons = 0;
+    //call our functions now
+    int linearIndex = linear_Search(testArr, N, testVal, linearComparisons);
+    int binIndex = binary_Search(testArr, N, testVal, binaryComparisons);
+
+    cout << "The test value was found at index = " << linearIndex << ", and took " << linearComparisons << 
+    " comparisons using the linear search algorithm.\n" ;
+
+    cout << "The test value was found at index = " << binIndex << ", and took " << binaryComparisons << 
+    " comparisons using the binary search algorithm.\n" ;
 
     return 0;
 }
